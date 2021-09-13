769ee1fd01231ae852151ff5bad36a13679792a0
ferminrp <frodriguezpenelas@gmail.com>
2021-09-12 22:23:56 -0300

    meta tags

--- src/jobs/jobPosting.svelte
+++ src/jobs/jobPosting.svelte
@@ -32,7 +32,7 @@
     border-radius: 0.5rem;
     padding: 0.5rem 2rem;
     box-sizing: border-box;
-    margin-top: 2rem;
+    margin-top: 1rem;
     cursor: pointer;
     display: inline-block;
   }
